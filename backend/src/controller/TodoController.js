8db6dcf5e9bdcf92e866dc41c98478c024d84fca
ssunny <ssssunny85@gmail.com>
2020-03-28 15:53:10 +0900

    목록 페이징 기능 추가

--- backend/src/controller/TodoController.js
+++ backend/src/controller/TodoController.js
@@ -2,30 +2,50 @@ const moment = require('moment');
 const Todo = require('../model/Todo');
 
 const todoController = {
-  list: (req, res) => {
+  list: async (req, res) => {
     const searchType = req.query.type;
     const searchText = req.query.text;
     const completed = req.query.completed;
-    const todo = Todo.find((err, todos) => {
-        if (err) return res.status(500).send(err);
-        res.json(todos);
-      });
+    const pageNo = parseInt(req.query.pageNo, 10) || 1;
+    const pageLimit = 3;
+
     if (searchType && searchText) {
       switch (searchType) {
         case 'content':
-          todo.regex(searchType, `.*${searchText}.*`);
+          Todo.regex(searchType, `.*${searchText}.*`);
           break;
         case 'createdAt':
-          todo.gte('createdAt', moment(searchText, 'YYYY-MM-DD').startOf('day').toDate().getTime());
-          todo.lte('createdAt', moment(searchText, 'YYYY-MM-DD').endOf('day').toDate().getTime());
+          Todo.gte('createdAt', moment(searchText, 'YYYY-MM-DD').startOf('day').toDate().getTime());
+          Todo.lte('createdAt', moment(searchText, 'YYYY-MM-DD').endOf('day').toDate().getTime());
           break;
       }
     }
     if (completed) {
-      todo.where('completed').equals(completed === 'true');
+      Todo.where('completed').equals(completed === 'true');
     }
-    todo
-      .sort({ id: -1 });
+
+    try {
+      const totalCount = await Todo.count();
+      const todos = await Todo
+        .find()
+        .limit(pageLimit)
+        .skip(pageLimit * (pageNo - 1))
+        .sort({id: -1})
+        .exec();
+
+      const result = {
+        paging: {
+          no: pageNo,
+          limit: pageLimit,
+          total: totalCount,
+        },
+        todos,
+      };
+
+      return res.json(result);
+    } catch (err) {
+      return res.status(500).send(err);
+    }
   },
   create: (req, res) => {
     const todo = new Todo(req.body);
